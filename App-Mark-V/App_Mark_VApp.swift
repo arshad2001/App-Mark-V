1fb4c0c6fcf0a44b31840b24bc4c236544aa101b
shadocode <62998839+arshad2001@users.noreply.github.com>
2024-06-29 17:25:37 +0700

    Initial Commit

---
+++ App-Mark-V/App_Mark_VApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_Mark_VApp.swift
+//  App-Mark-V
+//
+//  Created by Arshad Buchori on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct App_Mark_VApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
